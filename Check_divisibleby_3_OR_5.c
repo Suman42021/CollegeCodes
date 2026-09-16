d31f2caa557d3f43fb0f7a9ff237939de1b01eb5
JustSum4n <95698606+Suman42021@users.noreply.github.com>
2026-09-17 02:21:44 +0530

    Create Check_divisibleby_3_OR_5.c

---
+++ Check_divisibleby_3_OR_5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    printf("Enter number to check:");
+    scanf("%d", &x);
+    if (x%3 == 0)
+    {
+        printf("\nNumber is divisible by 3");
+    }
+    else
+    {
+        printf("\nNot divisible by 3");
+        
+    }
+    if (x%5 == 0)
+    {
+        printf("\nDivisible by 5");
+    }
+    else
+    {
+        printf("\nNot divisible by 5");
+    }
+    return 0;
+}
